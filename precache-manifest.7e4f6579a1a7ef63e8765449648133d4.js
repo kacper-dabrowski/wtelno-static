d3dc5b9efaa62a0f24b1d6eceeddc6b09179a213
kacper-cf <kacper@couponfollow.com>
2022-12-04 16:09:49 +0100

    add static files

---
+++ precache-manifest.7e4f6579a1a7ef63e8765449648133d4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde304b6a1ef4657266baf277f0ccb4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6cf21877cb0997ebee",
+    "url": "/static/css/main.4d7e71da.chunk.css"
+  },
+  {
+    "revision": "9291350354dda473ed1e",
+    "url": "/static/js/2.a739783c.chunk.js"
+  },
+  {
+    "revision": "65f1649126af1a71de334fc540fd6526",
+    "url": "/static/js/2.a739783c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6cf21877cb0997ebee",
+    "url": "/static/js/main.8c4500b5.chunk.js"
+  },
+  {
+    "revision": "55e0acd2ad859a98bfb7",
+    "url": "/static/js/runtime-main.5238f94f.js"
+  },
+  {
+    "revision": "e38de704d4dc10d6d245160bb2d7c0ef",
+    "url": "/static/media/calendar.e38de704.png"
+  },
+  {
+    "revision": "011bcd2608c9516a9fd042269223d132",
+    "url": "/static/media/church.011bcd26.jpg"
+  },
+  {
+    "revision": "fda6fc1b6a8c7c3b7bfe0854b2974ffa",
+    "url": "/static/media/church.fda6fc1b.jpg"
+  },
+  {
+    "revision": "be2e853c4fd27b6bcd98d18e0085e772",
+    "url": "/static/media/drzewowsvg.be2e853c.svg"
+  },
+  {
+    "revision": "739af70366c7d2c6385d297e8afcdb06",
+    "url": "/static/media/government.739af703.jpg"
+  },
+  {
+    "revision": "ea865324020a60771685e5f248a84c5b",
+    "url": "/static/media/government.ea865324.jpg"
+  },
+  {
+    "revision": "5b721f76ae4be43f5db82c92c01c70d4",
+    "url": "/static/media/main.5b721f76.jpg"
+  },
+  {
+    "revision": "5e8c8b71b9f1675255a49bc0b956f04e",
+    "url": "/static/media/main.5e8c8b71.jpg"
+  },
+  {
+    "revision": "2a491bd697780722c509622b022d55a5",
+    "url": "/static/media/pin.2a491bd6.png"
+  },
+  {
+    "revision": "83f4f61de1957f82fdf1f264b269370b",
+    "url": "/static/media/school.83f4f61d.jpg"
+  },
+  {
+    "revision": "eac2483d5ed93d2847fde7239a2a2779",
+    "url": "/static/media/school.eac2483d.jpg"
+  },
+  {
+    "revision": "82f449e2849cbd7542a30d1e69ebaf7e",
+    "url": "/static/media/scissors.82f449e2.png"
+  }
+]);
